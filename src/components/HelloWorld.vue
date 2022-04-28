e89427a62d31b6d1187f7917548a0281e0483205
sunyiran <sunyiran@zhihu.com>
2022-04-28 19:32:10 +0800

    feat: 增加样式

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -41,6 +41,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.hello {
+  padding: 10px;
+  border: 1px solid black;
+}
 h3 {
   margin: 40px 0 0;
 }
